436b0b0b1060f7d1556ad8d139660d8925889c48
Muhammad Haziq <beinghaziq@gmail.com>
2023-06-02 20:12:00 +0500

    initial commit

---
+++ src/test/java/com/example/currencyconvertionservice/CurrencyConvertionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currencyconvertionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConvertionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
