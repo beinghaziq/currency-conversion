23b4cdd42c9bf6e38b105252250091439d0c4ac2
Muhammad Haziq <beinghaziq@gmail.com>
2023-06-05 16:39:33 +0500

    Feign implemented for sending Api calls

---
+++ src/main/java/com/example/currencyconvertionservice/proxies/CurrencyExchangeProxy.java
@@ -0,0 +1,16 @@
+package com.example.currencyconvertionservice.proxies;
+
+import com.example.currencyconvertionservice.models.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.math.BigDecimal;
+
+@FeignClient(name="currency-exchange-service", url = "localhost:8000")
+public interface CurrencyExchangeProxy {
+  @GetMapping("/currency-exchange/from/{from}/to/{to}")
+  public CurrencyConversion retrieveExchangeValue(
+          @PathVariable String from,
+          @PathVariable String to);
+}
